1acd0585db85a99c20d4a362fc26b73285e21fb6
ceksioglu <64319097+ceksioglu@users.noreply.github.com>
2024-06-11 15:54:01 +0300

    Update CarView

--- src/view/CarView.java
+++ src/view/CarView.java
@@ -5,5 +5,14 @@ import javax.swing.*;
 public class CarView {
     private JPanel container;
     private JLabel label_head;
-    private JLabel label_;
+    private JLabel label_brand;
+    private JComboBox combo_brand;
+    private JLabel label_model;
+    private JComboBox combo_model;
+    private JLabel label_color;
+    private JTextField field_color;
+    private JTextField field_km;
+    private JLabel label_km;
+    private JTextField field_plate;
+    private JButton button_car_save;
 }
